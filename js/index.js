5b23877a81aa558be36c1ce558561e91954a82d8
DuHuaL <469580396@qq.com>
2020-09-20 23:58:40 +0800

    自动轮播加无缝滚动

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded',function() {
     // 在入口中实例化对象
     new Search('.jd_header_box');
+    new Banner('.jd_banner');
 });
 // 面向对象的方式写
 //1.搜索的构造函数
@@ -35,4 +36,85 @@ Search.prototype.init = function() {
     }
     that.el.style.background = `rgba(216,80,92,${opacity})`;
   }
+};
+
+// 轮播图
+//1.自动轮播 无缝滚动 无缝滑动
+//2、对应点的改变
+//3.滑动的效果
+//4、当滑动的距离不超过三分之一  吸附回去
+//5、当滑动的距离超过三分之一  切换图片（包括上一张  下一张）
+//6、根据体感比较快的速度  进行切换
+var Banner = function(selector) {
+  this.el = document.querySelector(selector);
+  // 宽度
+  this.width = this.el.offsetWidth;
+  // 图片容器
+  this.imgsBox = this.el.querySelector('ul:first-child');
+  // 点容器
+  this.listBox = this.el.querySelector('ul:last-child');
+  // 当前索引
+  this.index = 1;
+  // 初始化定时器
+  this.timeId = null;
+  // 调用初始化方法
+  this.init();
+};
+// 初始化
+Banner.prototype.init = function() {
+  this.autoplay();
+  this.seamless();
+};
+// 自动播放
+Banner.prototype.autoplay = function() {
+  var that = this;
+  // 根据当前索引每隔一段时间去切换
+  that.timeId = setInterval(function() {
+    that.index++;
+    // 过渡
+    that.addTransition();
+    // 位移
+    that.setTranslateX(-that.index * that.width);
+  },1000) 
+};
+// 无缝的衔接
+Banner.prototype.seamless = function() {
+  var that = this
+  //无缝滚动
+  // 监听第八张（索引为8）到第一张（索引是9）的切换 瞬间定位到第一张（索引1）
+  that.imgsBox.addEventListener('transitionend',function() {
+    // 当index为9 瞬间定位到索引1
+    if(that.index >= 9) {
+      that.index = 1;
+      //去掉过渡
+      that.removeTransition()
+      //在做位移
+      that.setTranslateX(-that.index * that.width);
+    }
+
+    // 无缝滑动
+    // 监听第1张（索引为1）到第8张（索引是0）的切换 瞬间定位到第8张（索引8）
+    else if(that.index <= 0) {
+      that.index = 8;
+      //去掉过渡
+      that.removeTransition();
+      //在做位移
+      that.setTranslateX(-that.index * width);
+    }
+  });
+};
+// 添加过渡
+Banner.prototype.addTransition = function() {
+  this.imgsBox.style.transition = 'all 0.2s';
+  this.imgsBox.style.webkitTransition = 'all 0.2s';
+};
+// 去除过渡
+Banner.prototype.removeTransition = function() {
+  this.imgsBox.style.transition = 'none';
+  this.imgsBox.style.webkitTransition = 'none';
+};
+// 位移
+Banner.prototype.setTranslateX = function(translateX) {
+  this.imgsBox.style.transform = 'translateX('+translateX+'px)';
+  this.imgsBox.style.webkitTransform = 'translateX('+translateX+'px)';
 };
